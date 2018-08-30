06a4f1fa459ddd21f1a6ca5f5d4c3b8450f2c90d
Oskar Kogut <kogutoskar123@gmail.com>
2018-08-30 16:49:08 +0200

    Added player movement

---
+++ and-laser-defender/Assets/scripts/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed = 25f;
+
+    [SerializeField]
+    float padding = 0.5f;
+
+    [SerializeField]
+    float yMaxPadding = 4f; 
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
+
+    private void Start()
+    {
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - yMaxPadding;
+    }
+
+    void Update () {
+        Move();
+	}
+
+    private void Move()
+    {
+        float deltaX = Input.acceleration.x * Time.deltaTime * moveSpeed;
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+
+        float deltaY = Input.acceleration.y * Time.deltaTime * moveSpeed;
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
